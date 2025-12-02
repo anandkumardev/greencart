394e7e664dfde1459f4330849a4d44960c57feb4
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2025-12-02 22:05:11 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import "dotenv/config";
+import userRouter from "./routes/userRoute.js";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+await connectDB();
+
+// Allow multiple origins
+const allowedOrigins = ["http://localhost:5173"];
+
+//Middlewares configuration
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+
+// User API Route
+app.use("/api/user", userRouter);
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
