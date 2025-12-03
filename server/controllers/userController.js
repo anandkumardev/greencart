bf90e8280f160f5f85038c8093b151000a568bd1
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2025-12-03 13:34:15 +0530

    update login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -43,7 +43,7 @@ export const register = async (req, res) => {
 };
 
 // Login User : /api/user/login
-export const login = async (res, req) => {
+export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
@@ -58,6 +58,28 @@ export const login = async (res, req) => {
     if (!user) {
       return res.json({ success: false, message: "Invaild email or password" });
     }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invaild email or password" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.json({
+      success: true,
+      user: { email: user.email, name: user.name },
+    });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: "Incorrect credentials." });
