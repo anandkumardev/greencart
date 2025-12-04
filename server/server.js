65d28a275c965bb0ccc0b6c4cf13c2c34300ddc6
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2025-12-04 17:50:00 +0530

    add API routes

--- server/server.js
+++ server/server.js
@@ -5,16 +5,22 @@ import connectDB from "./configs/db.js";
 import "dotenv/config";
 import userRouter from "./routes/userRoute.js";
 import sellerRouter from "./routes/sellerRoute.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import addressRouter from "./routes/addressRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 await connectDB();
+await connectCloudinary();
 
 // Allow multiple origins
 const allowedOrigins = ["http://localhost:5173"];
 
-//Middlewares configuration
+// Middlewares configuration
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: allowedOrigins, credentials: true }));
@@ -23,7 +29,20 @@ app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use("/api/user", userRouter);
 
 // Seller API Route
-app.use("api/seller", sellerRouter);
+app.use("/api/seller", sellerRouter);
+
+// Product API Route
+app.use("/api/product", productRouter);
+
+// Cart API Route
+app.use("/api/cart", cartRouter);
+
+// Address API Route
+app.use("/api/address", addressRouter);
+
+// Order API Route
+app.use("/api/order", orderRouter);
+
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
