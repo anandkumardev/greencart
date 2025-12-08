5b13de636da25df05d185e6b76846f5b269e4932
Anand Kumar <93657634+anandkumardev@users.noreply.github.com>
2025-12-08 14:03:56 +0530

    add domain

--- server/server.js
+++ server/server.js
@@ -19,7 +19,10 @@ await connectDB();
 await connectCloudinary();
 
 // Allow multiple origins
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://greencart.anandkumar.dev",
+];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
